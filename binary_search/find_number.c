49bd46be6fb29779ad8a4322373883561d169fd0
Uzo Ugochukwu <uugochukwu@aol.com>
2025-07-12 16:07:16 +0100

    completed, only works with numbers that ascend

--- binary_search/find_number.c
+++ binary_search/find_number.c
@@ -2,9 +2,9 @@
 
 int main() {
 
-  int number_list[] = {1,2,3,4,5};
+  int number_list[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50};
 
-  int target = 5;
+  int target = 12;
 
   int count = sizeof(number_list)/sizeof(number_list[0]);
 
@@ -22,13 +22,19 @@ int main() {
 
       median_pos = (gap + 1) / 2;
     }
-    
-    //printf("%d", median_pos);
-    
+      
 
     int med_change = 0;
 
-    for (int i = 0; i < count; i++){
+    for (int i = 0; i < 50; i++){
+
+      if(median_pos == 1) { // ideally if median is 1 we would do floor(1/2), but that requires doubles and I can't be bothered to change at this stage
+            if (number_list[median_pos] == target){
+	printf("%d", median_pos);
+	goto end;
+      }
+	    median_pos--;
+      } 
 
       if (number_list[median_pos] == target){
 	printf("%d", median_pos);
@@ -45,7 +51,7 @@ int main() {
 	med_change = gap/2;
 
 	median_pos +=med_change;
-	//printf("%d", median_pos);
+
       } else {
 
 	med_change = (gap+1)/2;
@@ -53,7 +59,6 @@ int main() {
 	median_pos +=med_change;
       }
 
-	
 
       } else {
 	
@@ -64,21 +69,15 @@ int main() {
       if ( (gap % 2) == 0 ) {
 	median_pos = gap/2;
 
-	//printf("%d", median_pos);
 
       } else {
 
 	median_pos = (gap+1)/2;
 
-
       }
-
-
 	
 
       }
-
-
       
 
     }
