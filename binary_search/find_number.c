d35634b52ce410d5fdc401275ca76da4ea54ba7a
Uzo Ugochukwu <uugochukwu@aol.com>
2025-07-12 15:31:08 +0100

    handles all situations besides searching for the first number in the array

--- binary_search/find_number.c
+++ binary_search/find_number.c
@@ -2,9 +2,9 @@
 
 int main() {
 
-  int number_list[] = {1,2,3,4,5,6,7,8,9};
+  int number_list[] = {1,2,3,4,5};
 
-  int target = 6;
+  int target = 5;
 
   int count = sizeof(number_list)/sizeof(number_list[0]);
 
@@ -12,11 +12,6 @@ int main() {
   int end_point = count -1;
 
   int median_pos = 0;
-  /*
-   if (number_list[median_pos] == target){
-      printf("%d", median_pos);
-      goto end;
-      }*/
 
   int gap = end_point - start_point;
 
@@ -27,55 +22,66 @@ int main() {
 
       median_pos = (gap + 1) / 2;
     }
-
-    int first_median = median_pos;
-
-for (int i = 0; i < count; i++){
-
-    if (number_list[median_pos] == target){
-      printf("%d", median_pos);
-      goto end;
-    }
-
-    if ( number_list[median_pos] > target ) {
-      end_point = median_pos;
-      start_point = 0;
-
-    } else {
-      end_point = count -1 -median_pos;
-      start_point = median_pos;
-    }
-
-    int gap = end_point - start_point;
+    
+    //printf("%d", median_pos);
     
 
-    
-    if (gap % 2 == 0) {
-      median_pos = gap/2;
-      
-      //must add start point // maybe add current median to first median
-      if (target < number_list[median_pos]) {
-      median_pos+=start_point;
-      }else {
-	median_pos+=median_pos;
+    int med_change = 0;
+
+    for (int i = 0; i < count; i++){
+
+      if (number_list[median_pos] == target){
+	printf("%d", median_pos);
+	goto end;
       }
 
-      
-    } else {
+      if (target > number_list[median_pos]) {
+	start_point = median_pos;
+	end_point = count - 1;
+	
+      gap = end_point - start_point;
 
-      median_pos = (gap + 1) / 2;
-      // must add start point
-      
-      if (target < number_list[median_pos]) {
-      median_pos+=start_point;
-      }else {
-	median_pos+=median_pos;
+      if ( (gap % 2) == 0 ) {
+	med_change = gap/2;
+
+	median_pos +=med_change;
+	//printf("%d", median_pos);
+      } else {
+
+	med_change = (gap+1)/2;
+
+	median_pos +=med_change;
       }
-      
-    }
 
-    
-  }
+	
+
+      } else {
+	
+	end_point = median_pos;
+	
+      gap = end_point - start_point;
+
+      if ( (gap % 2) == 0 ) {
+	median_pos = gap/2;
+
+	//printf("%d", median_pos);
+
+      } else {
+
+	median_pos = (gap+1)/2;
+
+
+      }
+
+
+	
+
+      }
+
+
+      
+
+    }
     
 
  end:
