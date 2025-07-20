8983e5d479d64a68faacfe27c3bb2ac6a6d3907e
Uzo Ugochukwu <uugochukwu@aol.com>
2025-07-20 23:19:23 +0100

    fixed weird array issue

--- depth_first_search/tree.c
+++ depth_first_search/tree.c
@@ -3,28 +3,18 @@
 int main() {
 
 char array[5][3] = {"S\0", "A\0", "B\0", "C\0", "D\0"};
- 
-//puts(array[3]);
 
 int length = sizeof(array)/sizeof(array[0]);
 
 char stack[5] = {};
 
-//printf("%d", length);
-
-//stack[0] = *array[0]; // *array[0] is the memory address
-
-//use memcpy to move data from array to stack
-
 for (int i = 0; i < length; i++){
   stack[i] = *array[i];
-  printf("%s\0", array[i]);
-  //printf("%d", i);
+  printf("%s", &array[i]);
+
   
  }
-//puts( &stack[2]);
-// printf("%s", &stack[0]); // the &stack[0] is now dereferencing that pointer
- 
+
 return 0;
 
 }
