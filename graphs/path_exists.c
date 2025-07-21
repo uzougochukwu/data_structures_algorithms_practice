0b511b766ec62b9b1e215cd84b9807b607fffa07
Uzo Ugochukwu <uugochukwu@aol.com>
2025-07-21 17:52:31 +0100

    second attempt

--- graphs/path_exists.c
+++ graphs/path_exists.c
@@ -1,30 +1,34 @@
 bool validPath(int n, int** edges, int edgesSize, int* edgesColSize, int source, int destination) {
-    // check all the arrays that contain the source
-    // that becomes new source`
-    // must have at least one element from previous
-    // array in current array
-    for (int i = 1; i < edgesSize; i++){
-        int first_prev = edges[i-1][0];
-        int second_prev = edges[i-1][1];
 
-        bool first_check;
-        bool second_check;
-
-        if ( (edges[i][0] != first_prev)  && (edges[i][0] != second_prev) ) {
-            first_check = false;
-        } else {
-            first_check = true;
-        }
-
-        if ( (edges[i][1] != first_prev)  && (edges[i][1] != second_prev) ) {
-            second_check = false;
-        } else {
-            second_check = true;
-        }
-
-        if ( (second_check == false) && (first_check == false) ) {
-            return false;
-        }
+bool containsNode(int position, int node){
+    if (edges[position][0] == node){
+        return true;
     }
-    return true;
+    if (edges[position][1] == node){
+        return true;
+    }
+    return false;
+}
+
+    // find all nodes that source connects with
+int find_number = source;
+
+for (int i = 0; i < edgesSize; i++){
+    if (containsNode(i, find_number) == true){
+        // check if other number is destination
+        if (containsNode(i, destination) == true) {
+            return true;
+        }
+        // now extract the other number
+        if (edges[i][0] == find_number){
+            find_number = edges[i][1];
+        } else {
+            find_number = edges[i][0];
+        }
+
+    }
+}
+return false;
+    // find all nodes that destination connects with
+
 }
