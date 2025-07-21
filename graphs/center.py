dbd2b52c9012cfb25a207bffae0ebf4aba7564a5
Uzo Ugochukwu <uugochukwu@aol.com>
2025-07-21 15:08:48 +0100

    graph center puzzle in python

---
+++ graphs/center.py
@@ -0,0 +1,47 @@
+'''
+There is an undirected star graph consisting of n nodes labeled from 1 to n. A star graph is a graph where there is one center node and exactly n - 1 edges that connect the center node with every other node.
+
+You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates that there is an edge between the nodes ui and vi. Return the center of the given star graph.
+
+ 
+
+Example 1:
+
+Input: edges = [[1,2],[2,3],[4,2]]
+Output: 2
+Explanation: As shown in the figure above, node 2 is connected to every other node, so 2 is the center.
+
+Example 2:
+
+Input: edges = [[1,2],[5,1],[1,3],[1,4]]
+Output: 1
+
+ '''
+
+
+edges = [[1,2],[2,3],[4,2]]
+
+class Solution:
+    def findCenter( edges) -> int:
+        
+        first = edges[0][0]
+        second = edges[0][1]
+        first_count = 0
+        second_count = 0
+
+        for x in edges:
+            for element in x:
+                if element == first:
+                    first_count += 1
+                if element == second:
+                    second_count += 1
+
+        if (first_count > second_count):
+            print("R")
+            return first
+        else:
+            print("Y")
+            return second
+        
+
+        findCenter([[1,2],[2,3],[4,2]])
