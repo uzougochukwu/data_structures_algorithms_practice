0cac95c1b91e81067c6ab4887d3484a0ea234e2e
Uzo Ugochukwu <uugochukwu@aol.com>
2025-08-10 15:44:39 +0100

    trimmed out comments, made note on how to use traversal through the linked list to replace create_new_node with add_node_at_end

--- linked_lists/3_ll.c
+++ linked_lists/3_ll.c
@@ -8,36 +8,6 @@ struct node {
 
 typedef struct node node_t;
 
-/*
-
-create the head node
-
-node_t *head = malloc(sizeof(node_t))
-
-head->value = 1;
-  
- */
-
-/*
-
-  pseudo code for function to create nodes
-
-  take in the new_value as a parameter
-
-  node_t *tmp = malloc(sizeof(node_t));
-  tmp->value = head->value;
-  tmp->next = head->next;
-
-  while (tmp->next != null)
-  {
-  
-  tmp->next = tmp->next->next;
-
-  }
-
-  tmp->value = new_value
-  
- */
 
 node_t* create_new_node(node_t *node_to_add_from, int new_value)
 {
@@ -51,8 +21,6 @@ node_t* create_new_node(node_t *node_to_add_from, int new_value)
   // set the value of the tmp node
   tmp->value = new_value;
 
-  // return the new node so that this can be continued
-  //return tmp; // try returning node_to_add_from instead
   return tmp;
 }
 
@@ -72,79 +40,28 @@ void print_list(node_t *current_node)
 
 int main() {
 
-  /*
-  node_t *o = malloc(sizeof(node_t));
-
-  o->value = 8;
-
-  node_t *i = malloc(sizeof(node_t));
-
-  o->next = i;
-
-  i->value = 10;
-
-  printf("The next pointer of o is %d and the address of i is %d\n", o->next, i);
-
-  node_t *tmp = malloc(sizeof(node_t));
-
-  tmp->value = o->value;
-   
-  printf("The first value is %d\n", tmp->value);
-   */
-
-  /*
-  create_new_node(head, 5);
-
-  int current = head->next->value;
-
-  printf("Second value is %d\n", current);
-   */
-
   node_t *head = malloc(sizeof(node_t));
-
-  node_t *tmp, *final;
 
   head->value = 1;
 
+  // modify create_new_node to be add_node_at_end, traverse linked list till the end, then add the node
+
+  node_t *tmp;
+
   tmp = create_new_node(head, 2);
 
-  final = create_new_node(tmp, 3);
-  
-  /*
-  node_t *tmp = malloc(sizeof(node_t));
+  node_t *over;
 
-  tmp->value = 2;
+  over = create_new_node(tmp, 3);
 
-  head->next = tmp;
+  node_t *again;
 
-  node_t *use = malloc(sizeof(node_t));
+  again = create_new_node(over, 4);
 
-  use->value = 3;
+  node_t *even;
 
-  tmp->next = use;
+  even = create_new_node(again, 5);
 
-  printf("second value is %d\n", head->next->value);
-  */
-  
-  
-  /*
-  for (int i = 2; i < 5; i++)
-    {
-      node_t *tmp = head;
-
-      head = create_new_node(tmp, i); // constantly feeding in the current head, need to feed in the result of the last function
-
-      printf("value is %d\n", head->value);
-
-       // must move the next pointer
-
-       //head->next = head;
-     
-       }*/
-
-  // create_new_node is replacing the head value, rather than linking it up
-
-  //printf(" final value is %d\n", head->value);
 
   print_list(head); // print_list works somewhat
 
