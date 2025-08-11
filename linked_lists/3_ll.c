e63be9bbbf3b8722940f908105025949ee314d10
Uzo Ugochukwu <uugochukwu@aol.com>
2025-08-11 17:57:13 +0100

    produced a loop that can automatically extend the linked list, using the add_node_at_end function, also removed comments and the useless create_node function

--- linked_lists/3_ll.c
+++ linked_lists/3_ll.c
@@ -9,21 +9,6 @@ struct node {
 typedef struct node node_t;
 
 
-node_t* create_new_node(node_t *node_to_add_from, int new_value)
-{
-
-  // create node using malloc
-  node_t *tmp = malloc(sizeof(node_t));
-
-  // link the node_to_add_from with the tmp node
-  node_to_add_from->next = tmp;
-
-  // set the value of the tmp node
-  tmp->value = new_value;
-
-  return tmp;
-}
-
 void print_list(node_t *current_node)
 {
   while (current_node->next != NULL)
@@ -65,32 +50,12 @@ int main() {
 
   head->value = 1;
 
-  add_node_at_end(head, 2);
+  for (int i = 2; i < 7; i++){
+    add_node_at_end(head, i);
 
-  add_node_at_end(head, 3);
+  }
 
-  add_node_at_end(head, 4);
-
-  // modify create_new_node to be add_node_at_end, traverse linked list till the end, then add the node, print list shows how to traverse
-  /*
-  node_t *tmp;
-
-  tmp = create_new_node(head, 2);
-
-  node_t *over;
-
-  over = create_new_node(tmp, 3);
-
-  node_t *again;
-
-  again = create_new_node(over, 4);
-
-  node_t *even;
-
-  even = create_new_node(again, 5);
-  */
-
-  print_list(head); // print_list works somewhat
+  print_list(head); 
 
   return 0;
 }
