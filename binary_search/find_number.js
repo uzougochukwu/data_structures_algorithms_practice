56077c094b18bb7ae4060d354b1540dd3a02dc4c
Uzo Ugochukwu <uugochukwu@aol.com>
2025-10-21 23:15:49 +0100

    some explanatory comments

--- binary_search/find_number.js
+++ binary_search/find_number.js
@@ -7,11 +7,17 @@ const binary_search = (array, value) => {
 
     while (left <= right){
 
-	mid = Math.floor(left + (right - left)/2); // works for both floor and ceiling, explain why
+	mid = Math.floor(left + (right - left)/2); // works for both floor and ceiling
+	// if you calculate mid without floor or ceil and get a fraction, that means that right and left are next to each other, since the gap is only 1
+	// depending on whether you use ceil or floor, the mid can become equal to the right or left respectively
+	// if you use ceil, mid becomes the right - if the value at mid is the search value, exit. if the value at mid is still more than search value, then we move the right pointer one space left and now both right and left are on the same value - the next check is the final check before exit with 'did not find'
+	// if you use floor, mid becomes the left - if the value at mid is the search value, exit. if the value at mid is still less than the search value, then we move the left pointer one space right and now both right and left are on the same value - the next check is the final check before exit with 'did not find'
 
 	if (array[mid] == value){
 
-	    break;
+	    console.log(`Your value is in position ${mid}`); 
+
+	    return;
 	}
 
 	if (array[mid] > value) {
@@ -24,8 +30,8 @@ const binary_search = (array, value) => {
 
     }
 
-    console.log(`Your value is in position ${mid}`); 
+    console.log("The value was not found");
 }
 
-binary_search(sorted_array, -1);
+binary_search(sorted_array, 4);
 
