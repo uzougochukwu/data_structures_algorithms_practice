1e6c6c97d7f09c5d4419317f3f93e07442e10e08
Uzo Ugochukwu <uugochukwu@aol.com>
2025-10-22 10:51:24 +0100

    some js tricks

---
+++ practice_js/demo.js
@@ -0,0 +1,58 @@
+// function getCoordinates(){
+
+//     const x = 10;
+//     const y = 20;
+//     return [x, y];
+
+// }
+
+// const [x,y] = getCoordinates();
+// console.log(x,y)
+
+// const nums = [10, 1, 5];
+// nums.sort((x, y) => y - x);
+// console.log(nums);
+
+// const arr = [1, 2, 2, 3];
+
+// // const set = new Set(arr);
+
+// const uniqueArr = [...new Set(arr)];
+
+// console.log(uniqueArr);
+
+// function beg(request = 'follow'){
+//     console.log(`Please, ${request}!`);
+// }
+// beg();
+
+// const obj = { a: 1, b: { c: 2 } };
+
+// const clone = structuredClone(obj)
+
+// //clone.b.c = 5;
+
+// console.log(clone);
+
+// const arr = new Array(10).fill('')
+// console.log(arr);
+
+// const arr = [2, 4, 8];
+
+// const hasEven = arr.every((val) => val % 2 === 0);
+
+// console.log(hasEven);
+
+// const nums = [1, 2, 3, 4, 5, 0];
+
+// const max = Math.max(...nums);
+
+// console.log(max);
+
+let a = 5;
+
+let b = 10;
+
+[a, b] = [b, a];
+
+console.log(a, b);
