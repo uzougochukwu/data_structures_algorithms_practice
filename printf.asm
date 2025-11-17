93087cda9e2a56b119db6b0d6958ca059e433e35
Uzo Ugochukwu <uugochukwu@aol.com>
2025-11-17 21:59:47 +0000

    printf improved to work with any 64 bit number

--- printf.asm
+++ printf.asm
@@ -1,12 +1,3 @@
-	; only prints up to 2559
-	; if you try to print 2560, when you divide by 10 the quotient is 256, which exceeds the maximum space of al, which is the register that stores the quotient
-	; need way to detect it if the first digit is 0
-	; 
-	
-section .bss
-	number resb 1
-	
-
 section .data
 
 global _start
@@ -14,88 +5,56 @@ global _start
 section .text
 
 _start:
-	xor rax, rax
-	xor rdx, rdx
 
+	xor rdx, rdx		; zero out rdx as the remainder is stored here (which we will print)
+	xor r12, r12		; keep track of no. of digits to print
 
-	mov ax, 0xAA
+	mov rax, 0xFFFFFFFFFFFFFFFF		; dividend goes in rax, this is what we will print
+	mov rdi, 0xA
 
-	; rsp contains roughly 0x7fffffffff, rbp contains 0
+	sub rsp, 0x4
 
-	mov rbp, rsp		; stack pointer and base pointer should point to same memory location
-
-	; push 10 on stack to tell function when to end, 10 means '\n'
-	; might not need this as rcx keeps track of the length being pushed
-
-	mov byte [rsp], 0xA			; everytime we push a value onto the stack, the stack pointer is decremented. the value we pushed occupies the lowest portion of the stack
-	; eventually, the most significant digit in the number will occupy the very lowest portion of the stack, we then use the write syscall
-
-	mov r12, 0x0
-
-	mov sil, 0xA		; rax will be divided by rdi
-
-OnStackLoop:
-	xor rbx, rbx
-
-	div sil		; remainder goes in ah and quotient goes in al
-
-	; this is a 16 bit dividend in ax being divided by an 8 bit divisor in sil, so the quotient goes in al and the remainder goes in ah
-
-	; check that quotient is not zero, because if it is that means we are on the final digit of the number (the least significant)
-	cmp al, 0x0
+put_digits_on_stack:
+	cmp rax, 0		; if the final digit is zero, it means one of two things: 1.) we already printed the last digit 2.) the last digit is 0
 	je last_digit
 
-	add ah, 0x30		; to convert the remainder into its ascii equivalent
+	div rdi			; rax / rdi , quotient (the result of the division) goes in rax
+	; rdx has the remainder
 
-	mov bh, ah		; can't push an 8 bit value onto the stack so must mov it into bh (the high 8 bits of the first 16 bits of rbx) first
-	shr rbx, 8
-	sub rsp, 4
-	mov byte [rsp], bl
+	add rdx, 0x30		; add 0x30 so it is converted to an ascii text value of the number
 
-	sub ah, 0x30		; back to normal
-	
+	mov dword [rsp], edx	; dword as 32 bit
 
-	inc r12
+	xor rdx, rdx		; division is rdx:rax divided by rdi, so we must zero rdx after each divide, since that is where the remainder is stored
 
-	; ah still contains remainder, which will confound our next division
-	; use bitmask to remove the value in ah but preserve value in al
-	and ax, 255
+	sub rsp, 0x4		; move rsp down 4 bytes, as 32 bit int
 
-	jmp OnStackLoop
+	inc r12			; increase r12 as we have moved a digit to the stack for printing
 
+	jmp put_digits_on_stack
 
 last_digit:
-	add ah, 0x30
-	
-	mov bh, ah
-	shr rbx, 8
+	add rdx, 0x30
+	mov dword [rsp], edx
 
-	sub rsp, 4
-	mov byte [rsp], bl	
 	inc r12
 
-	; r12 contains the number of digits in the number as a whole
+	add rsp, 0x4
 
-ActualPrint:
-	
-	;	mov eax, dword [rsp]
+print:
 
-	cmp r12, 0 		; if we have printed the last digit, then exit
+	cmp r12, 0x0
 	je exit
-	
-	mov rax, 0x1
+
 	mov rdi, 0x1
-	mov rdx, 0x1
+	mov rax, 0x1
 	mov rsi, rsp
+	mov rdx, 0x1
 	syscall
 
-	add rsp, 4
-
+	add rsp, 0x4
 	dec r12
-
-	jmp ActualPrint
-	
-
+	jmp print
 
 exit:
 	
