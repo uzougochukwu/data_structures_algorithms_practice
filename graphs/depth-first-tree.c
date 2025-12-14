2b51b79eb74f7154fde00238605762b6a85ded32
Uzo Ugochukwu <uugochukwu@aol.com>
2025-12-14 20:37:43 +0000

    plan for bcd

--- graphs/depth-first-tree.c
+++ graphs/depth-first-tree.c
@@ -15,25 +15,25 @@ TreeNode* new_node(int val){
   return n;
 }
 void print_depth_first(TreeNode* root){
-  TreeNode* process_stack[100];
-  size_t process_stack_num = 0;
-  process_stack[0] = root;
-  process_stack_num++;
+  TreeNode* process_stack[100];  // stack of TreeNode pointers, max 100
+  size_t process_stack_num = 0;  // keeps track of how many elements in the stack
+  process_stack[0] = root;      // first element is the root node
+  process_stack_num++;          // increment process_stack_num to show we have 1 element
 
-  while (process_stack_num > 0){
-    TreeNode* current = process_stack[process_stack_num - 1];
-    process_stack_num--;
+  while (process_stack_num > 0){ // whilst the process_stack has at least 1 element
+    TreeNode* current = process_stack[process_stack_num - 1]; // the current node is taken from the end of the stack
+    process_stack_num--;    // decrement process_stack_num as we have taken an element
 
-    printf("%d ", current->val);
+    printf("%d ", current->val);   // print the current node
   
 
-    if (current->right != NULL){
-      process_stack[process_stack_num] = current->right;
-      process_stack_num++;
+    if (current->right != NULL){   // check that the right node exists
+      process_stack[process_stack_num] = current->right;  // if it does, add it to the stack of TreeNode pointers
+      process_stack_num++;      // increment process_stack_num as we have added an element
     }
-    if (current->left != NULL){
-      process_stack[process_stack_num] = current->left;
-      process_stack_num++;
+    if (current->left != NULL){  // check that the left node exists
+      process_stack[process_stack_num] = current->left;   // if it does, add it to the stack of TreeNode pointers
+      process_stack_num++;    // increment process_stack_num as we have added an element
     }
   }
 }
@@ -49,8 +49,10 @@ int main(int argc, char* argv[]){
 
   root->left = new_node(1);
   root->right = new_node(2);
+  
   root->left->left = new_node(3);
   root->left->right = new_node(4);
+
   root->right->left = new_node(5);
   root->right->right = new_node(6);
 
