fd2da785f9289e2b83a6681d9a1b135a5df0be1a
Uzo Ugochukwu <uugochukwu@aol.com>
2025-12-18 16:06:13 +0000

    print_regs finished

--- include/print_registers.asm
+++ include/print_registers.asm
@@ -2,8 +2,8 @@
 %include "../include/printf.asm"
 
 section .data
-	registers dq "rax:rdi:rsi:rbx:rcx:"
-	print_line db "\n"
+	registers dq "rax:rdi:rsi:rbx:rcx:rdx:rbp:rsp:r8r:r9r:r10:r11:r12:r13:r14:r15:"
+	print_line db `\n`
 	
 section .text
 
@@ -271,13 +271,740 @@ rbx_print:
 	pop rbx
 	pop rax
 
+rcx_print:
+
+	; save all regs
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	push rsi
+	push rdi
+	push rbp
+	push rsp
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+
+	; save the reg to print in this section
+
+	push rcx
+	
+	mov rsi, registers
+
+	add rsi, 16
+
+	call write_reg_to_screen
+
+	
+	; restore the reg to print in this section
+
+	pop rcx
+
+	mov rax, rcx		; move rcx into rax for printing
+
+	xor rdx, rdx
+
+	mov qword [rsp],rax
+	call printf
+
+	; restore all regs
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rsp
+	pop rbp
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+
+
+rdx_print:
+
+	; save all regs
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	push rsi
+	push rdi
+	push rbp
+	push rsp
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+
+	; save the reg to print in this section
+
+	push rdx
+	
+	mov rsi, registers
+
+	add rsi, 20
+
+	call write_reg_to_screen
+
+	
+	; restore the reg to print in this section
+
+	pop rdx
+
+	mov rax, rdx		; move rdx into rax for printing
+
+;	xor rdx, rdx
+
+	mov qword [rsp],rax
+	call printf
+
+	; restore all regs
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rsp
+	pop rbp
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+
+rbp_print:
+
+	; save all regs
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	push rsi
+	push rdi
+	push rbp
+	push rsp
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+
+	; save the reg to print in this section
+
+	push rbp
+	
+	mov rsi, registers
+
+	add rsi, 24
+
+	call write_reg_to_screen
+
+	
+	; restore the reg to print in this section
+
+	pop rbp
+
+	mov rax, rbp		; move rbp into rax for printing
+
+	xor rdx, rdx
+
+	mov qword [rsp],rax
+	call printf
+
+	; restore all regs
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rsp
+	pop rbp
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+
+rsp_print:
+
+	; save all regs
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	push rsi
+	push rdi
+	push rbp
+	push rsp
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+
+	; save the reg to print in this section
+
+	push rsp
+	
+	mov rsi, registers
+
+	add rsi, 28
+
+	call write_reg_to_screen
+
+	
+	; restore the reg to print in this section
+
+	pop rsp
+
+	mov rax, rsp		; move rsp into rax for printing
+
+	xor rdx, rdx
+
+	mov qword [rsp],rax
+	call printf
+
+	; restore all regs
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rsp
+	pop rbp
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+
+r8_print:
+
+	; save all regs
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	push rsi
+	push rdi
+	push rbp
+	push rsp
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+
+	; save the reg to print in this section
+
+	push r8
+	
+	mov rsi, registers
+
+	add rsi, 32
+
+	call write_reg_to_screen
+
+	
+	; restore the reg to print in this section
+
+	pop r8
+
+	mov rax, r8		; move r8 into rax for printing
+
+	xor rdx, rdx
+
+	mov qword [rsp],rax
+	call printf
+
+	; restore all regs
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rsp
+	pop rbp
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+
+
+r9_print:
+
+	; save all regs
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	push rsi
+	push rdi
+	push rbp
+	push rsp
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+
+	; save the reg to print in this section
+
+	push r9
+	
+	mov rsi, registers
+
+	add rsi, 36
+
+	call write_reg_to_screen
+
+	
+	; restore the reg to print in this section
+
+	pop r9
+
+	mov rax, r9		; move r9 into rax for printing
+
+	xor rdx, rdx
+
+	mov qword [rsp],rax
+	call printf
+
+	; restore all regs
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rsp
+	pop rbp
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+
+
+r10_print:
+
+	; save all regs
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	push rsi
+	push rdi
+	push rbp
+	push rsp
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+
+	; save the reg to print in this section
+
+	push r10
+	
+	mov rsi, registers
+
+	add rsi, 40
+
+	call write_reg_to_screen
+
+	
+	; restore the reg to print in this section
+
+	pop r10
+
+	mov rax, r10		; move r10 into rax for printing
+
+	xor rdx, rdx
+
+	mov qword [rsp],rax
+	call printf
+
+	; restore all regs
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rsp
+	pop rbp
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+
+
+r11_print:
+
+	; save all regs
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	push rsi
+	push rdi
+	push rbp
+	push rsp
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+
+	; save the reg to print in this section
+
+	push r11
+	
+	mov rsi, registers
+
+	add rsi, 44
+
+	call write_reg_to_screen
+
+	
+	; restore the reg to print in this section
+
+	pop r11
+
+	mov rax, r11		; move r11 into rax for printing
+
+	xor rdx, rdx
+
+	mov qword [rsp],rax
+	call printf
+
+	; restore all regs
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rsp
+	pop rbp
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+
+r12_print:
+
+	; save all regs
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	push rsi
+	push rdi
+	push rbp
+	push rsp
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+
+	; save the reg to print in this section
+
+	push r12
+	
+	mov rsi, registers
+
+	add rsi, 48
+
+	call write_reg_to_screen
+
+	
+	; restore the reg to print in this section
+
+	pop r12
+
+	mov rax, r12		; move r12 into rax for printing
+
+	xor rdx, rdx
+
+	mov qword [rsp],rax
+	call printf
+
+	; restore all regs
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rsp
+	pop rbp
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+
+r13_print:
+
+	; save all regs
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	push rsi
+	push rdi
+	push rbp
+	push rsp
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+
+	; save the reg to print in this section
+
+	push r13
+	
+	mov rsi, registers
+
+	add rsi, 52
+
+	call write_reg_to_screen
+
+	
+	; restore the reg to print in this section
+
+	pop r13
+
+	mov rax, r13		; move r13 into rax for printing
+
+	xor rdx, rdx
+
+	mov qword [rsp],rax
+	call printf
+
+	; restore all regs
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rsp
+	pop rbp
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+
+
+r14_print:
+
+	; save all regs
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	push rsi
+	push rdi
+	push rbp
+	push rsp
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+
+	; save the reg to print in this section
+
+	push r14
+	
+	mov rsi, registers
+
+	add rsi, 56
+
+	call write_reg_to_screen
+
+	
+	; restore the reg to print in this section
+
+	pop r14
+
+	mov rax, r14		; move r14 into rax for printing
+
+	xor rdx, rdx
+
+	mov qword [rsp],rax
+	call printf
+
+	; restore all regs
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rsp
+	pop rbp
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+
+
+r15_print:
+
+	; save all regs
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	push rsi
+	push rdi
+	push rbp
+	push rsp
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+
+	; save the reg to print in this section
+
+	push r15
+	
+	mov rsi, registers
+
+	add rsi, 60
+
+	call write_reg_to_screen
+
+	
+	; restore the reg to print in this section
+
+	pop r15
+
+	mov rax, r15		; move r14 into rax for printing
+
+	xor rdx, rdx
+
+	mov qword [rsp],rax
+	call printf
+
+	; restore all regs
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rsp
+	pop rbp
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+
+
 end_pr:
 
 	; print new_line
 	mov rax, 0x1
 	mov rdi, 0x1
 	mov rsi, print_line
-	mov rdx, 0x1
+	mov rdx, 0x2
 	syscall
 
 	
