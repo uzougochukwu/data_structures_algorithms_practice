8aa74df73f2a74e000f70e8964ebf9f7651f9734
Uzo Ugochukwu <uugochukwu@aol.com>
2025-12-20 10:19:17 +0000

    changed printf include path

--- include/print_registers.asm
+++ include/print_registers.asm
@@ -1,5 +1,5 @@
 
-%include "../include/printf.asm"
+%include "/home/calebmox/data_structures_algorithms_practice/include/printf.asm"
 
 section .data
 	registers dq "rax:rdi:rsi:rbx:rcx:rdx:rbp:rsp:r8r:r9r:r10:r11:r12:r13:r14:r15:"
