943b9d6daf2f1e855764cb99ed6476766b65e0e3
Uzo Ugochukwu <uugochukwu@aol.com>
2025-12-27 07:24:44 +0000

    practice breadth first

--- graphs/tree2.c
+++ graphs/tree2.c
@@ -54,7 +54,34 @@ void print_depth_first_recursive(TreeNode* root){
   }
 }
 
+void print_breadth_first(TreeNode* root){
+  TreeNode* process_queue[100];
+  size_t process_queue_num = 0;
 
+  process_queue[0]=root;
+  process_queue_num++;
+
+  while (process_queue_num>0){
+    int i;
+    TreeNode* current = process_queue[0];
+
+    for (i=0;i<process_queue_num-1;i++){
+      process_queue[i]=process_queue[i+1];
+    }
+    process_queue_num--;
+
+    printf("%d ", current->val);
+
+    if( current->left != NULL){
+      process_queue[process_queue_num] = current->left;
+      process_queue_num++;
+  }
+    if( current->right != NULL){
+      process_queue[process_queue_num] = current->right;
+      process_queue_num++;
+    }
+}
+}
 
 
 int main(){
